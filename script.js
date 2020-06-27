dea46f9c5390531ca1013666df79e2cf4fe33ca9
Mateus Santos <m.santos.azeve@gmail.com>
2020-06-26 23:00:41 -0300

    preparing for css

--- script.js
+++ script.js
@@ -4,23 +4,23 @@ function carregar(){
     var img = window.document.getElementById('imagem')
     var dataH = new Date()
     var dataM = new Date()
-    var hora = 0//dataH.getHours()
+    var hora = dataH.getHours()
     var minutos = dataM.getMinutes()
     var nome = 'Mateus'//window.prompt("Olá! Eu sou o site criado pelo Vinícius. Qual o seu nome?")
     
-    if(hora>=6 && hora<=12){
+    if(hora > 5 && hora <= 12){
         //bom dia
         msg.innerHTML = `<h2>Bom dia, ${nome}!</h2>`
         msg.innerHTML += `<h3>Agora são ${hora} horas e ${minutos} minutos.</h3>`
         img.src = 'fotomanha.png'
         document.body.style.background = '#fed364'
-    } else if(hora>12 && hora<18){
+    } else if(hora > 12 && hora < 18){
         //boa tarde
         msg.innerHTML = `<h2>Boa tarde, ${nome}!</h2>`
         msg.innerHTML += `<h3>Agora são ${hora} horas e ${minutos} minutos.</h3>`
         img.src = 'fototarde.png'
         document.body.style.background = '#ee9a46'
-    } else if(hora>=18 && hora<=23){
+    } else if(hora >= 18 && hora <= 23){
         //boa noite
         msg.innerHTML = `<h2>Boa noite, ${nome}!</h2>`
         msg.innerHTML += `<h3>Agora são ${hora} horas e ${minutos} minutos.</h3>`
